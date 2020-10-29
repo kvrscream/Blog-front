a8f90033d4dc552b5c8150f7ebc68a2303f64efa
Mac Kainos <kainos@MacBook-Air-de-Mac.local>
2020-10-29 10:05:29 -0300

    telas de cadastro de usuário e menu adm mais axios

---
+++ src/components/menuAdmin.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const MenuAdmin = () => {
+    return (
+        <div>
+            <ul id="slide-out" className="sidenav sidenav-fixed">
+                <li>
+                    <div className="user-view">
+                        <div className="background">
+                            <img src="images/office.jpg" />
+                        </div>
+                        <a href="#user"><img className="circle" src="images/yuna.jpg" /></a>
+                        <a href="#name"><span className="white-text name">Felipe Botelho</span></a>
+                        <a href="#email"><span className="white-text email">botelho422@gmail.com</span></a>
+                    </div>
+                </li>
+                <li>
+                    <Link to="/listaPosts"><i className="material-icons">cloud</i>Posts</Link>
+                </li>
+                <li><div className="divider"></div></li>
+                <li>
+                    <Link to="/usuarios" className="subheader">Usuários</Link>
+                </li>
+            </ul>
+            <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+        </div>
+    )
+}
+
+export default MenuAdmin;
--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -1,10 +1,81 @@
 import React, {Component} from "react";
+import api from "../api/api";
+import M from "materialize-css";
+
 
 class Cadastro extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name: "",
+            email:"",
+            password: ""
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+
+    handleName = (event) =>{
+        this.setState({name: event.target.value});
+    }
+
+    handleEmail = (event) =>{
+        this.setState({email: event.target.value});
+    }
+
+    handlePassword = (event) =>{
+        this.setState({password: event.target.value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        let data = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password
+        }
+        api.post("/users/create", data).then((response) => {
+            this.toast(response.data.message);
+        }).catch((erro) => {
+            console.log("Erro => ", erro);
+        })
+    }
+
+    toast = (msg) => {
+        M.toast({html: ""})
+    }
+
     render(){
         return(
             <div className="container">
-
+                <div className="row">
+                    <div className="col s12">
+                        <h3>Cadastro de usuário</h3>
+                    </div>
+                    <form className="col s8">
+                        <div className="input-field col s12">
+                            <i className="material-icons prefix">account_circle</i>
+                            <input id="icon_prefix" type="text" className="" onChange={this.handleName} />
+                            <label for="icon_prefix">Nome</label>
+                        </div>
+                        <div className="input-field col s12">
+                            <i className="material-icons prefix">email</i>
+                            <input id="icon_prefix" type="text" className="" onChange={this.handleEmail} />
+                            <label for="icon_prefix">E-mail</label>
+                        </div>
+                        <div className="input-field col s12">
+                            <i className="material-icons prefix">lock</i>
+                            <input id="icon_prefix" type="password" className="" onChange={this.handlePassword} />
+                            <label for="icon_prefix">Senha</label>
+                        </div>
+                        <div className="col s12">
+                            <a className="waves-effect waves-light btn" onClick={this.handleSubmit}>Salvar</a>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
