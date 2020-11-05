7ffabdeee93af0b210a18f0091784edfa1770f63
Mac Kainos <kainos@MacBook-Air-de-Mac.local>
2020-11-05 09:28:12 -0300

    menu lateral

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,23 +1,36 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import csharp from "../images/csharp.jpg";
+import html from "../images/html.png";
 
 const Menu = () => {
     return (
         <div>
             <ul id="slide-out" className="sidenav sidenav-fixed">
-                <li><div className="user-view">
-                <div className="background">
-                    <img src="images/office.jpg" />
-                </div>
-                <a href="#user"><img className="circle" src="images/yuna.jpg" /></a>
-                <a href="#name"><span className="white-text name">Felipe Botelho</span></a>
-                <a href="#email"><span className="white-text email">botelho422@gmail.com</span></a>
-                </div></li>
-                <li><Link to="/login"><i className="material-icons">cloud</i>Proxima Página</Link></li>
-                <li><a href="#!">Second Link</a></li>
+                <li>
+                    <div className="user-view">
+                        <div className="background">
+                            <img src={csharp} className="" />
+                        </div>
+                        <a href="#user"><img className="circle" src={html} /></a>
+                        <a href="#name"><span className="white-text name">Felipe Botelho</span></a>
+                    </div>
+                </li>
+                <li>
+                    <Link to="/">
+                        <i className="material-icons">code</i>Posts
+                    </Link>
+                </li>
                 <li><div className="divider"></div></li>
-                <li><a className="subheader">Subheader</a></li>
-                <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+                <li>
+                    <a className="subheader">Mais</a>
+                </li>
+                <li>
+                    <a className="waves-effect" href="#!">Sobre</a>
+                </li>
+                <li>
+                    <a className="waves-effect" href="#!">Contato</a>
+                </li>
             </ul>
             <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
         </div>
